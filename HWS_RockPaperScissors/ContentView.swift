c899c1b7deac85e10cff40bebaed4d3077d965c9
systemsevenfive <ryanwsaunders@icloud.com>
2023-02-26 11:00:44 -0500

    Initial Commit

---
+++ HWS_RockPaperScissors/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HWS_RockPaperScissors
+//
+//  Created by Ryan Saunders on 2023-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HWS_RockPaperScissors/HWS_RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_RockPaperScissorsApp.swift
+//  HWS_RockPaperScissors
+//
+//  Created by Ryan Saunders on 2023-02-26.
+//
+
+import SwiftUI
+
+@main
+struct HWS_RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
